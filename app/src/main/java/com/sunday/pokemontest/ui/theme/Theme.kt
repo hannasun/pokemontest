62aa04f79da0cac2f5d04974042fb98d9a2ff093
hannasun <sun936@foxmail.com>
2026-05-29 18:26:25 +0800

    delete useless code

--- app/src/main/java/com/sunday/pokemontest/ui/theme/Theme.kt
+++ app/src/main/java/com/sunday/pokemontest/ui/theme/Theme.kt
@@ -1,38 +1,12 @@
 package com.sunday.pokemontest.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
 val PokemonYellow = Color(0xFFFFCB05)
 val PokemonBlue = Color(0xFF3B5BA8)
 val PokemonBlueDark = Color(0xFF2A4080)
